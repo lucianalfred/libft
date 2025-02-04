23e1d9cc654aede685d424f0498c1c5141228182
Luciano Alfredo <lucianalfred60@gmail.com>
2025-02-04 12:01:23 +0100

    4 functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,26 +1,28 @@
 #include "libft.h"
+#include <stdio.h>
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int signal;
-    int number;
+	int	i;
+	int	sign;
+	int	nb;
 
-    i = 0;
-    while()
-        i++;
-    signal = 1;
-    if(str[i] == '-' || str[i] == '+')
-    {
-        if(str[i] == '-')
-            signal *= -1;
-        i++;
-    }
-    number = 0;
-    while(ft_isdigit(str[i]))
-    {
-        number = (number * 10) + (str[i] - '0');
-        i++;
-    }
-    return (number * signal);
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	sign = 1;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	nb = 0;
+	while (ft_isdigit(str[i]))
+	{
+		nb = nb * 10 + str[i] - '0';
+		i++;
+	}
+	return (nb * sign);
 }
